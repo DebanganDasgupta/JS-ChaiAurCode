8794822367ce9b0051f51cce6a81a2040ad5f428
DebanganDasgupta <debangandasguptaofficial@gmail.com>
2026-08-01 20:43:43 +0000

    new 01_Variables files

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+let accountEmail = "debangan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata" 
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity])
